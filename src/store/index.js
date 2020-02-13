24e70e461c4bbcf70a13418d69edfdef485decb1
Sokurenko <Sergii.Sokurenko@dnvgl.com>
2020-02-13 16:36:56 +0100

    Persisitng state in localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,20 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 
 import { counterSlice } from './slices/counter'
 
+const preloadedState = localStorage.getItem('preloadedState')
+  ? JSON.parse(localStorage.getItem('preloadedState'))
+  : {}
+
 const store = configureStore({
   reducer: {
     counter: counterSlice.reducer
   },
-  middleware: [...getDefaultMiddleware()]
+  middleware: [...getDefaultMiddleware()],
+  preloadedState
 })
+
+store.subscribe(() =>
+  localStorage.setItem('preloadedState', JSON.stringify(store.getState()))
+)
 
 export default store
